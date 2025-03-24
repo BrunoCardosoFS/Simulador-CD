1402fd91c89ea8d7b777aebf10d55151844ce202
Bruno Cardoso <bc13092002@gmail.com>
2025-03-24 12:17:21 -0300

    build version 0.0.1.5

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ compile_with_nuitka(
     pyfile=pyfile,
     product_name="Simulador Circuitos Digitais",
     output_filename="simulador",
-    file_version="0.0.1.4",
+    file_version="0.0.1.5",
     file_description="Simulador Circuitos Digitais",
     copyright="Copyright 2024 Bruno Cardoso",
     icon=icon,
